3952df05e5aea08a3a1852c84b49421b1a49137a
Hoa0104 <hoa.2474802010118@vanlanguni.vn>
2025-11-20 23:27:08 +0700

    Delete navbar.php

--- navbar.php
+++
@@ -1,9 +0,0 @@
-<nav style="background:#c19a6b; padding:10px;">
-  <a href="index.php">Trang chủ</a> |
-  <a href="customer/menu.php">Menu</a> |
-  <a href="#">Giới thiệu</a> |
-  <a href="#">Liên hệ</a> |
-  <a href="#">Ngôn ngữ</a> |
-  <a href="#">Tin tức</a> |
-  <a href="customer/feedback.php">Chăm sóc khách hàng</a>
-</nav>
